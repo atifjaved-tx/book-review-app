573ba1ff712006d89e2bd53951f9c39340517669
qarabraza-tx <qarabraza@techcreatix.com>
2022-12-09 19:46:59 +0500

    add image with activestorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  get 'authors/index'
+  devise_for :users
+  root "authors#index"
+
+  # resource:author
+   resources :authors
+
+
+   devise_scope :user do
+    authenticated :user do
+    root 'author#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+     # root to: "devise/sessions#new"
+
+  end
+  
+   # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
